d8d76cb8d354222e18b4c64b65ab18b26386de4b
Paris Dolfman <parisdolfman@gmail.com>
2019-01-16 20:51:34 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -15,6 +15,7 @@ const codes = [
 
 function init() {
   let index = 0
+<<<<<<< HEAD
 
   document.body.addEventListener("keydown", (e) => {
     const key = e.key
@@ -22,6 +23,14 @@ function init() {
     index = (codes[index] === key) ? ++index : 0
 
 
+=======
+  
+  document.body.addEventListener('keydown', function(e) => {
+    const key = e.key;
+    if (codes[index] === key) {
+     index++
+     index = 0;
+>>>>>>> 8374a057b06b21a82f8fa16296c2773c599d0d1a
      if (index === codes.length) {
        window.alert("Hurray!");
        index = 0
